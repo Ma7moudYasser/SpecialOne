850aa5ceba877e811c15794e7c969554fcda2b51
Mahmoud Yasser <57537704+Ma7moudYasser@users.noreply.github.com>
2022-10-19 19:48:03 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import imageio
+import scipy.ndimage
+import cv2
+import imageio.v2 as imageio
+
+img = "aya.png"
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, .1140])
+
+
+def dodge(front, back):
+    # if image is greater than 255 (which is not possible) it will convert it to 255
+    final_sketch = front * 255 / (255 - back)
+    final_sketch[final_sketch > 255] = 255
+    final_sketch[back == 255] = 255
+
+    # to convert any suitable existing column to categorical type we will use aspect function
+    # and uint8 is for 8-bit signed integer
+    return final_sketch.astype('uint8')
+
+
+ss = imageio.imread(img)
+gray = rgb2gray(ss)
+
+i = 255 - gray
+
+# to convert into a blur image
+blur = scipy.ndimage.filters.gaussian_filter(i, sigma=13)
+
+# calling the function
+r = dodge(blur, gray)
+
+cv2.imwrite('4.png', r)
